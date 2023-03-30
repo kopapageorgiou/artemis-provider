e08e95ce73f1994496e085e9acba204d66e1bf51
Konstantinos Papageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-03-30 13:03:03 +0300

    ✨feature[🟠 Api]: Add endpoint to insert sensors in scylla db by devPapas

---
+++ provider/rest_api/app/routers/Sensors.py
@@ -0,0 +1,29 @@
+import os
+from fastapi import APIRouter
+from pydantic import BaseModel
+from cassandra.cluster import Cluster
+
+
+cluster = Cluster([os.environ['DB_HOST']])
+router = APIRouter()
+
+class Sensor(BaseModel):
+    sensor_id: str
+    sensor_type: str
+    gateway_id: str
+
+@router.post('/insertSensor')
+def insert_client(sensor: Sensor):
+    try:
+        session = cluster.connect('mkeyspace')
+        query = '''INSERT INTO sensors
+        (sensor_id, sensor_type, gateway_id)
+        VALUES (%s, %s, %s)'''
+        values = (sensor.gateway_id,
+                  sensor.sensor_type,
+                  sensor.gateway_id)
+        session.execute(query=query, parameters=values)
+        session.shutdown()
+        return {"info": "Sensor has been imported successfully", "code": 1}
+    except Exception as e:
+        return {"info": e, "code": 0}
