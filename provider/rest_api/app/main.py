7f9ec2d240cca852afba389c5f342897ad1eff71
Konstantinos Papageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-03-30 13:03:47 +0300

    ✨feature[🟠 Api]: Add routers for endpoints by devPapas

--- provider/rest_api/app/main.py
+++ provider/rest_api/app/main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from cassandra.cluster import Cluster
+from routers import Clients, Gateways, Sensors, Measurements
 import os
 
 cluster = Cluster([os.environ['DB_HOST']])
 
 app = FastAPI()
 
-@app.post('/hello')
+app.include_router(Clients.router)
+app.include_router(Gateways.router)
+app.include_router(Sensors.router)
+app.include_router(Measurements.router)
+
+@app.post('/test')
 def hello():
     try:
         session = cluster.connect('mkeyspace')
-        query= '''SELECT * FROM sensors'''
+        query= '''SELECT * FROM clients'''
         rows = session.execute(query=query)
-        return{"progress": [row for row in rows]}
+        return{"values from clients table": [row for row in rows]}
     except Exception as e:
         return {"exception": e}
