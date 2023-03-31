6efd8dc9285738005bdf34388b278a0670812aac
Konstantinos Papageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-03-31 13:28:07 +0300

    🖥️ wip(🟠 Api): Add endpoint to test orbitdb by devPapas

--- provider/rest_api/app/main.py
+++ provider/rest_api/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from cassandra.cluster import Cluster
 from routers import Clients, Gateways, Sensors, Measurements
-import os
+from orbitdbapi import OrbitDbAPI
+import os, sys
+import logging
+#import ipfshttpclient as ipfs
+import requests
 
 cluster = Cluster([os.environ['DB_HOST']])
 
@@ -20,4 +24,17 @@ def hello():
         rows = session.execute(query=query)
         return{"values from clients table": [row for row in rows]}
     except Exception as e:
+        return {"exception": e}
+    
+@app.post('/testOrbitdb')
+def hello():
+    try:
+        #ipfs_client = ipfs.connect('/dns/172.21.0.2/tcp/5001/http')
+        client = OrbitDbAPI(base_url=f"http://172.21.0.2:5001")
+        print(os.environ['ORBIT_HOST'], file=sys.stderr)
+        db = client.db('testOrbitdbfgdgf')
+        print("here2", file=sys.stderr)
+        return{"Databases": client.list_dbs()}
+    except Exception as e:
+        print(e.args, file=sys.stderr)
         return {"exception": e}
