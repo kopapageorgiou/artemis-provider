9ad08e5d5f29706525983ee1f21e5e8daf97e436
kopapageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-05-15 10:47:50 +0000

    🖥️ wip[🟠 Api]: orbitdb api call to save measurements works by devPapas

--- provider/rest_api/app/routers/Measurements.py
+++ provider/rest_api/app/routers/Measurements.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from fastapi import APIRouter
 from pydantic import BaseModel
 from cassandra.cluster import Cluster
@@ -7,11 +7,15 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 import base64
 import requests
+import logging
+parent_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_folder_path)
+from Orbitdbapi import OrbitdbAPI
 
 
 cluster = Cluster([os.environ['DB_HOST']])
 router = APIRouter()
-
+logging.basicConfig(level=logging.DEBUG)
 class Measurement(BaseModel):
     measurement_id: int
     measurement_value: int
@@ -26,17 +30,39 @@ flags = {}
 def insert_client(measurement: Measurement):
     try:
         session = cluster.connect('mkeyspace')
-        query = '''INSERT INTO gateways
-        (measurement_id, measurement_value, measurement_timestamp, measurement_location, current_stop_id, sensor_id)
-        VALUES (%s, %s, %s, %s)'''
-        values = (measurement.gateway_id,
-                  measurement.gateway_description,
-                  measurement.vehicle_id,
-                  measurement.client_id)
+        logging.debug("0")
+        query = '''INSERT INTO measurements
+        (measurement_id, current_stop_id, measurement_location, measurement_time, measurement_value, sensor_id)
+        VALUES (%s, %s, %s, %s, %s, %s)'''
+        logging.debug("00")
+        values = (measurement.measurement_id,
+                    measurement.current_stop_id,
+                    measurement.measurement_location,
+                    measurement.measurement_timestamp,
+                    measurement.measurement_value,
+                    measurement.sensor_id)
+        logging.debug("01")
+        logging.debug("0a")
         session.execute(query=query, parameters=values)
+        logging.debug("0b")
         session.shutdown()
-        return {"info": "Measurement has been imported successfully", "code": 1}
+        logging.debug("1")
+        orbitdb = OrbitdbAPI(orbithost=os.environ['ORBIT_HOST'], port=3000)
+        logging.debug("2")
+        res = orbitdb.insertMeasurements({
+            "order_id": 2,
+            "measurement_id": measurement.measurement_id,
+            "sensor_id": measurement.sensor_id,
+            "enc_measurement_value": measurement.measurement_value,
+            "enc_measurement_time": measurement.measurement_timestamp,
+            "enc_measurement_location": measurement.measurement_location,
+            "abe_enc_key": 'test',
+            
+        })
+        logging.debug("3")
+        return {"info": "Measurement has been imported successfully", "code": 1, "debug": res}
     except Exception as e:
+        logging.debug(e)
         return {"info": e, "code": 0}
 
 def checkStop(vehicle_id, stop_id):
