ba774c341bc05eafcb5ad59196ead58ada6cb9b3
kopapageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-05-18 11:41:15 +0000

    🔵 other[🟠 Api]: Comment out testorbitdb endpoint by devPapas

--- provider/rest_api/app/main.py
+++ provider/rest_api/app/main.py
@@ -25,30 +25,31 @@ def hello():
         return{"values from clients table": [row for row in rows]}
     except Exception as e:
         return {"exception": e}
-    
-@app.post('/testOrbitdb')
-def hello():
-    try:
-        print(os.environ['ORBIT_HOST'], file=sys.stderr)
-        orbitdb = OrbitdbAPI(orbithost=os.environ['ORBIT_HOST'], port=3000)
-        db = orbitdb.load(dbname='test-base1')
-        print(db, file=sys.stderr)
-        res = db.insert(data={"key": 1, "name": "testName"})
-        print(res, file=sys.stderr)
-        res = db.query(query={"attribute": "name", "operator": "eq", "value": "testName"})
-        print(res, file=sys.stderr)
-        res = db.getAll()
-        print(res, file=sys.stderr)
-        #client = OrbitDbAPI(base_url=f"https://{os.environ['ORBIT_HOST']}:3000",timeout=60)
-        #print(res.json(), file=sys.stderr)
+
+
+# @app.post('/testOrbitdb')
+# def hello():
+#     try:
+#         print(os.environ['ORBIT_HOST'], file=sys.stderr)
+#         orbitdb = OrbitdbAPI(orbithost=os.environ['ORBIT_HOST'], port=3000)
+#         db = orbitdb.load(dbname='test-base1')
+#         print(db, file=sys.stderr)
+#         res = db.insert(data={"key": 1, "name": "testName"})
+#         print(res, file=sys.stderr)
+#         res = db.query(query={"attribute": "name", "operator": "eq", "value": "testName"})
+#         print(res, file=sys.stderr)
+#         res = db.getAll()
+#         print(res, file=sys.stderr)
+#         #client = OrbitDbAPI(base_url=f"https://{os.environ['ORBIT_HOST']}:3000",timeout=60)
+#         #print(res.json(), file=sys.stderr)
         
-        # with open('/etc/ssl/certs/X509Certificate.pem', 'rb') as infile:
-        #     customca = infile.read()
-        # with open(cafile, 'ab') as outfile:
-        #     outfile.write(b'\n')
-        #     outfile.write(customca)
-         #print(db, file=sys.stderr)
-        return{"Database": db}
-    except Exception as e:
-        print(e.args, file=sys.stderr)
-        return {"exception": e}
+#         # with open('/etc/ssl/certs/X509Certificate.pem', 'rb') as infile:
+#         #     customca = infile.read()
+#         # with open(cafile, 'ab') as outfile:
+#         #     outfile.write(b'\n')
+#         #     outfile.write(customca)
+#          #print(db, file=sys.stderr)
+#         return{"Database": db}
+#     except Exception as e:
+#         print(e.args, file=sys.stderr)
+#         return {"exception": e}
