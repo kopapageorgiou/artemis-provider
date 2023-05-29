0626971198f9b0aa0f75273616b951324767454b
kopapageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-05-29 11:04:32 +0000

    🐞 fix[🟠 Api]: Modify the way endpoint works depending on the publishing mqtt data by devPapas

--- provider/rest_api/app/routers/Measurements.py
+++ provider/rest_api/app/routers/Measurements.py
@@ -1,5 +1,5 @@
 import os, sys
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from pydantic import BaseModel
 from cassandra.cluster import Cluster
 from Crypto.Cipher import AES
@@ -18,6 +18,7 @@ router = APIRouter()
 logging.basicConfig(level=logging.DEBUG)
 class Measurement(BaseModel):
     order_id: str #Must be known from the caller
+    gateway_id: int
     measurement_id: int
     measurement_value: int
     measurement_timestamp: str
@@ -26,12 +27,13 @@ class Measurement(BaseModel):
     sensor_id: str
 
 flags = {}
-keys = {}
 
 @router.post('/insertMeasurement')
-def insert_client(measurement: Measurement):
+def insert_client(measurement: Measurement, request: Request):
     try:
+        logging.debug("1a")
         session = cluster.connect('mkeyspace')
+        logging.debug("1b")
         query = '''INSERT INTO measurements
         (measurement_id, current_stop_id, measurement_location, measurement_time, measurement_value, sensor_id)
         VALUES (%s, %s, %s, %s, %s, %s)'''
@@ -42,18 +44,31 @@ def insert_client(measurement: Measurement):
                     measurement.measurement_timestamp,
                     measurement.measurement_value,
                     measurement.sensor_id, )
+        logging.debug("1c")
         session.execute(query=query, parameters=values)
+        logging.debug("1d")
         query = '''SELECT * FROM sensors WHERE sensor_id = %s'''
-        gateway_id = session.execute(query, (measurement.sensor_id,)).one().gateway_id
-        query = '''SELECT vehicle_id FROM gateways WHERE gateway_id = %s'''
-        vehicle_id = session.execute(query, (gateway_id,)).one().vehicle_id
+        logging.debug("1e")
+        #gateway_id = session.execute(query, (measurement.sensor_id,)).one().gateway_id
+        gateway_id = measurement.gateway_id
+        logging.debug("1f")
         session.shutdown()
-        checkStop(vehicle_id, int(measurement.current_stop_id))
-        enc_measurement_value = encrypt(keys[vehicle_id], str(measurement.measurement_value))
-        enc_measurement_time = encrypt(keys[vehicle_id], measurement.measurement_timestamp)
-        enc_measurement_location = encrypt(keys[vehicle_id], measurement.measurement_location)
+        logging.debug("1g")
+        #checkStop(vehicle_id, int(measurement.current_stop_id))
+        logging.debug(request.app.state.gate_keys)
+        key = request.app.state.gate_keys[gateway_id]
+        logging.debug("1")
+        #logging.debug(key)
+        enc_measurement_value = encrypt(key, str(measurement.measurement_value))
+        logging.debug("2")
+        enc_measurement_time = encrypt(key, measurement.measurement_timestamp)
+        logging.debug("3")
+        enc_measurement_location = encrypt(key, measurement.measurement_location)
+        logging.debug("4")
         orbitdb = OrbitdbAPI(orbithost=os.environ['ORBIT_HOST'], port=3000)
+        logging.debug("5")
         db = orbitdb.load(dbname='shared.measurements')
+        logging.debug("6")
         res = db.insertMeasurements({
             "order_id": measurement.order_id,
             "measurement_id": measurement.measurement_id,
@@ -61,14 +76,16 @@ def insert_client(measurement: Measurement):
             "enc_measurement_value": enc_measurement_value,
             "enc_measurement_time": enc_measurement_time,
             "enc_measurement_location": enc_measurement_location,
-            "abe_enc_key": keys[vehicle_id]
+            "abe_enc_key": key
             
         })
+        logging.debug("7")
         return {"info": "Measurement has been imported successfully", "code": 1, "debug": res}
     except Exception as e:
         logging.debug(e)
         return {"info": e, "code": 0}
 
+# ! DEPRICATED! Probably will get removed
 def checkStop(vehicle_id, stop_id):
     logging.debug("Checking for stop")
     if vehicle_id in flags:
@@ -82,7 +99,7 @@ def checkStop(vehicle_id, stop_id):
         logging.debug("vehicle register 1st time")
         flags[vehicle_id] = 0
         keys[vehicle_id] = generateSymmetricKey()
-    
+# ! DEPRICATED! Probably will get removed    
 def generateSymmetricKey():
     logging.debug("Generating key")
     return base64.b64encode(get_random_bytes(32)).decode('utf-8')
