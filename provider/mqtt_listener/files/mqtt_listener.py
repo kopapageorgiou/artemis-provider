b8906a7e423fde319db3bb7aee772784cecbf117
kopapageorgiou <kwnstantinospapageorgiou@gmail.com>
2023-05-29 11:00:40 +0000

    ✨feature[🔵 Scripts]: Mqtt listener currently calls /insertMeasurement on data published by devPapas

--- provider/mqtt_listener/files/mqtt_listener.py
+++ provider/mqtt_listener/files/mqtt_listener.py
@@ -1,21 +1,43 @@
 import paho.mqtt.client as mqtt
-import time
-import os
-
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+import time, json, os, requests, random
 
 broker_address= os.environ['MQTT_BROKER']
+REST_API_ADDRESS = "http://"+os.environ['REST_API']+":8000"
 port = 1883
+
+def on_message(client, userdata, message):
+    topic = message.topic.split("/")
+    client_id = topic[0]
+    gateway_id = topic[1]
+    payload_type = topic[2]
+    if payload_type == "data":
+        p1 = json.loads(str(message.payload.decode("utf-8")))['data'][0]
+        payload = json.loads(p1)
+        #print(measurement_id)
+        attrs = {
+            "order_id": f"{random.Random().randint(1000,2000)}",
+            "gateway_id": payload['gate_id'],
+            "measurement_id": int(f"{payload['gate_id']}{payload['serial']}"),
+            "measurement_value": payload['temp'],
+            "measurement_timestamp": payload['timestamp'],
+            "measurement_location": f"{random.Random().uniform(-90.0, 90.0)}, {random.Random().uniform(-180.0, 180.0)}",
+            "current_stop_id": "0",
+            "sensor_id": str(f"{payload['gate_id']}{payload['name_temp']}")
+        }
+        response = requests.post(REST_API_ADDRESS+"/insertMeasurement", json=attrs)
+        print(response.json())
+    else:
+        payload = json.loads(str(message.payload.decode("utf-8")))['event'][0]
+    print("client id =", client_id)
+    print("gateway_id =", gateway_id)
+    print("type =", payload_type)
+    print("payload =", payload)
 
 print("creating new instance")
 client = mqtt.Client("P1", clean_session=False)
 client.on_message=on_message
 print("connecting to broker")
 client.connect(broker_address, port)
-print("Subscribing to topic","testTopic/testData")
-client.subscribe("testTopic/testData")
+print("Subscribing to topic","")
+client.subscribe("#")
 client.loop_forever()
